a29144d005b71b450bae0be048fc491e5681e2af
M KHOIRUDDIN <khorud@yahoo.com>
2021-11-23 15:46:01 +0700

    Delete autokaya.sh

--- autokaya.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
-tar xf xmrig-6.14.1-linux-x64.tar.gz
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:9wbB8baHkzHDZK8hFptGRfjH8cog8dzExh.khorud2 -p x
